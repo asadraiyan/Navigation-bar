c753bf1dbbfa0ce0c1c3ba5550da980d3fc27100
Asad Raiyan <asadraiyan001@gmail.com>
2024-05-02 15:06:32 +0530

    updated

--- src/Components/About/components/AboutImage.js
+++ src/Components/About/components/AboutImage.js
@@ -5,7 +5,7 @@ const AboutImage = () => {
       <div className="about-content">
         <h1 className="intro">A little bit about myself !</h1>
         <p className="intro-desc">
-          I'm a mechanical engineer, worked in my core field for about 2.5 years
+          I'm a mechanical engineer, worked in my core field for about 2 years
           and then i started learning coding in my free time but later on day by
           day my passion towards coding is increasing and then out of curiosity
           i joined web development course where i upgraded my skils and made
